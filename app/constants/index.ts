9c0329e16400ce4bf8f32e538ae521a58c0c0fdc
imdova <ahmed.habib2030@gmail.com>
2024-12-24 10:41:37 +0200

    Update index.ts

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -3,17 +3,17 @@ import { Post } from "@/types";
 export const posts: Post[] = [
   {
     image: "/images/poter.jpg",
-    name: "Sales and tele-sales Representative",
+    name: "Sales and tele-sales Rep",
     features: [
       "Bachelor Degree",
       "Male&Female",
       "Mid Level",
-      "Experience (3-5) Yrs",
-      "Age (23-28)",
+      "Experience (1-3) Yrs",
+      "Age (23-27)",
     ],
     budget: "Confidential",
 
-    url: "https://medicova.com",
+    url: "https://docs.google.com/forms/d/e/1FAIpQLSfJ711QkFqnltmx3GtckQjfApBJdNl_9wJ9IlTqMyRYBCraaw/viewform?usp=sf_link",
     location: "Nasr City, Cairo",
     jobTime: "Full Time | On Site",
   },
